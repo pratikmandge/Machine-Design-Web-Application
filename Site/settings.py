75bf08a7da67cf533da5afe95e32096c761041d9
Pratik Mandge <88229260+pratikmandge@users.noreply.github.com>
2021-12-25 11:03:08 +0530

    Update settings.py

--- Site/settings.py
+++ Site/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'Site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': BASE_DIR / 'db.postgresql',
+        'USER': 'rkdidoelqg',
+        'PASSWORD': 'Machine@1234',
     }
 }
 
